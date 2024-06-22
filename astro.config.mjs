8ade11542ef3cbd355ec53fc9dd1da92e03bdc14
Todd Bittner <toddbittner@gmail.com>
2024-06-22 14:14:11 -0500

    Added an Astro island.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://withastro.com',    
+  site: 'https://withastro.com',
+  integrations: [preact()]
 });
